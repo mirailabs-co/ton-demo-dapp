40f47d27459c8fcbecbd320650b58b8d497e3137
Tien Tran <tvtienit@gmail.com>
2024-10-16 09:18:20 +0700

    chore: update dapp manifestUrl

--- src/connector.ts
+++ src/connector.ts
@@ -3,8 +3,7 @@ import { notification } from 'antd';
 import { isMobile, openLink } from 'src/utils';
 
 const dappMetadata = {
-	manifestUrl:
-		'https://gist.githubusercontent.com/siandreev/75f1a2ccf2f3b4e2771f6089aeb06d7f/raw/d4986344010ec7a2d1cc8a2a9baa57de37aaccb8/gistfile1.txt',
+	manifestUrl: 'https://miraiapp-ton-demo.web.app/tonconnect-manifest.json',
 };
 
 export const connector = new TonConnect(dappMetadata);
